dfd3b5714b2f2bf986ec3cb27c54afa8d726cda7
jannpey <hfw6915@autuni.ac.nz>
2022-05-15 23:42:35 +0800

    Deleted some information in Homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,9 +2,9 @@ import React from "react";
 const Home = () => {
  return (
  <div>
- <h2> How to use SPEED</h2>
- <p>I am not sure what to put on the home page</p>
- <p>A nice logo? Instructions?</p>
+
+ <h2> Home Page</h2>
+ 
  </div>
  );
  }
