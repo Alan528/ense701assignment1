8e37f5afc653ae0246681fba63be13cc5e95f65d
jannpey <hfw6915@autuni.ac.nz>
2022-05-15 23:14:51 +0800

    Added more information In About Us

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -3,10 +3,12 @@ import React from "react";
 const About = () => {
  return (
  <div>
- <h2> About Us</h2>
- <p> How to use SPEED</p>
- <p>Search Article: You can use to search Authors, Title or Publiction Year</p>
- <p>Submit Article: Submit a new article by entering the Title, Author, Publiction of Year and DOI.</p>
+     
+ <h2 style={{textAlign: "center"}}> About Us</h2>
+ <br></br>
+ <p style={{textAlign: "center", fontSize: "25px"}}> How to use SPEED</p>
+ <p><b>Search Article:</b> You can use to search Authors, Title or Publiction Year</p>
+ <p><b>Submit Article:</b> Submit a new article by entering the Title, Author, Publiction of Year and DOI.</p>
  
  </div>
  );
