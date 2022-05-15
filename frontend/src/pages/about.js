c42e1e4e77e19938ce21f032b3e82020b885c77e
jannpey <hfw6915@autuni.ac.nz>
2022-05-15 21:33:23 +0800

    Added information im about us

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -4,6 +4,9 @@ const About = () => {
  return (
  <div>
  <h2> About Us</h2>
+ <p> How to use SPEED</p>
+ <p>Search Article: You can use to search Authors, Title or Publiction Year</p>
+ <p>Submit Article: Submit a new article by entering the Title, Author, Publiction of Year and DOI.</p>
  
  </div>
  );
