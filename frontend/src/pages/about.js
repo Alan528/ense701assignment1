d946a4df117521980e001fab4db6a4836cc3559f
jannpey <hfw6915@autuni.ac.nz>
2022-05-16 17:20:10 +0800

    Changes Information in About Us

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -7,7 +7,7 @@ const About = () => {
  <h2 style={{textAlign: "center"}}> About Us</h2>
  <br></br>
  <p style={{textAlign: "center", fontSize: "25px"}}> How to use SPEED</p>
- <p><b>Home Page :</b> Will show random articles.</p>
+ <p><b>Home Page:</b> Will show random articles.</p>
  <p><b>Search Article:</b> You can use to search Authors, Title or Publiction Year.</p>
  <p><b>Submit Article:</b> Submit a new article by entering the Title, Author, Publiction of Year and DOI.</p>
  
