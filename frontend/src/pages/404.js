e5dc5911da1eadb8cc0d2b8fbb6e3dd3b47eee67
Alan528 <83804199+Alan528@users.noreply.github.com>
2022-05-17 19:58:20 +0800

    Make some change

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
-class NotFo
+class NotFoundPage extends Component {
+    render() {
+    return (
+    <div>
+        <h2> Error 404.  This page does not exist</h2>
+        <p>Select from the menu.</p>
+        </div>
     );
 }
 } 
