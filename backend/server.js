fdf476a2f347ff36637904ea15ffa984c357595e
jannpey <67101174+jannpey@users.noreply.github.com>
2022-05-29 11:15:32 +1200

    Updated Server.js and installed build

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,17 @@ server.get('/', (req, res) => {
 
 server.use('/api/articles', articles);
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+// Step 2:
+app.get("*", function (request, response) {
+
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+
+});
+
 server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
