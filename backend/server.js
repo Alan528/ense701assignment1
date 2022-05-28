df93584d9c37e32490c0548840c1d8af219d1b0f
jannpey <67101174+jannpey@users.noreply.github.com>
2022-05-29 11:30:55 +1200

    Updated server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ server.use('/api/articles', articles);
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+server.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // Step 2:
-app.get("*", function (request, response) {
+server.get("*", function (request, response) {
 
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 
