28518fa24efbfa78d1931f2f87fccea5ba043099
jannpey <67101174+jannpey@users.noreply.github.com>
2022-05-29 11:34:14 +1200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,6 @@ server.use(express.json({ extended: false }));
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-server.get('/', (req, res) => {
-    res.send("API is running")
-})
-
 server.use('/api/articles', articles);
 
 // Accessing the path module
